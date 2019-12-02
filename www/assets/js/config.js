7bb42262c74ad32d52a407c6fcf6f8788cb7a659
basmulder03 <bas.d.mulder@gmail.com>
2019-12-02 11:58:21 +0100

    change config

--- www/assets/js/config.js
+++ www/assets/js/config.js
@@ -1,6 +1,6 @@
 FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is107_5.RrpMz3jYoZkxYVRq",
-    database: "fys_is107_5",
-    environment: "mockup"
+    database: "fys_is107_5_dev",
+    environment: "dev"
 });
