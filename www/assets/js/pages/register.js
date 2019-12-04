00383a3bb9a5b1f9c3904cbe6130aae777e285c0
basmulder03 <bas.d.mulder@gmail.com>
2019-12-04 15:03:23 +0100

    Encryption js
    Verplaats salt naar encryption js en voeg md5 encryption toe aan de encription js bestand

--- www/assets/js/pages/register.js
+++ www/assets/js/pages/register.js
@@ -89,25 +89,3 @@ function register() {
         console.log(reason);
     });
 }
-
-//genereer salt
-function salt(length) {
-  //constantes van alle unicode tekens die waaruit de salt mag bestaan.
-  const ASCII_MIN = 48,
-        ASCII_MAX = 122;
-  let salt = '';
-  let random;
-  //loop door het aantal karakters heen waaruit de salt moet bestaan
-  for (let i = 0; i <= length; i++) {
-    //black list codes
-    do {
-      //genereer een random nummer
-      random = Math.floor(Math.random() * (ASCII_MAX - ASCII_MIN) + ASCII_MIN);
-      if ((random < 58 || random > 64) && (random < 91 || random > 96)) {
-        //zet het getal om tot een ascii karakter, voeg deze daarna toe aan de salt string
-        salt += String.fromCharCode(random);
-      }
-    } while ((random > 58 && random < 64) || (random > 91 & random < 96))
-  }
-  return salt;
-}
