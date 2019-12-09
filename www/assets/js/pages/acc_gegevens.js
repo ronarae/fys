6888a9bb38d8c0c3237cd644dfd666a95e67c236
Anies Sayed <aniesjunior@outlook.com>
2019-12-09 16:14:12 +0100

    update

--- www/assets/js/pages/acc_gegevens.js
+++ www/assets/js/pages/acc_gegevens.js
@@ -1,18 +1,19 @@
 //functie accGegevensUpdate
 function accGegevensUpdate() {
+    console.log('update');
     let voornaam = $("#voornaam").val();
     let tussenvoegsel = $("#tussenvoegsel").val();
     let achternaam = $("#achternaam").val();
 //    achternaam = encrypt(achternaam, salt);
     let geslacht = $("#geslacht").val();
     let geboorte_datum = $("#geboorte_datum").val();
-    let gebruiker_id = fyscloud.session.get("userId")
+    let gebruiker_id = FYSCloud.Session.get("UserId");
 
-
+    console.log(voornaam, achternaam, geslacht, geboorte_datum);
    //de ingevoerde waardes worden in de database gezet
        FYSCloud.API.queryDatabase(
-           "UPDATE gebruiker SET voornaam = ?, tussenvoegsel = ? achternaam = ?, geslacht = ?, geboorte_datum = ?, WHERE gebruiker_id = ?",
-           [voornaam, tussenvoegsel, achternaam, geslacht, geboorte_datum, gebruiker_id]
+           "UPDATE gebruiker SET voornaam = ?, tussenvoegsel = ? achternaam = ?, geslacht = ?, geboorte_datum = ?, WHERE gebruiker_id = 2",
+           [voornaam, tussenvoegsel, achternaam, geslacht, geboorte_datum]
        ).done(function(data) {
            console.log(data);
        }).fail(function(reason) {
