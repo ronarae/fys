1b1c4fea274de9f888a1b9f3897d7a176cce9f61
Anies Sayed <aniesjunior@outlook.com>
2019-12-09 14:49:54 +0100

    accgegevens

--- www/assets/js/pages/acc_gegevens.js
+++ www/assets/js/pages/acc_gegevens.js
@@ -1,51 +1,22 @@
 //functie accGegevensUpdate
 function accGegevensUpdate() {
-console.log("mn method werkt");
     let voornaam = $("#voornaam").val();
     let tussenvoegsel = $("#tussenvoegsel").val();
     let achternaam = $("#achternaam").val();
 //    achternaam = encrypt(achternaam, salt);
     let geslacht = $("#geslacht").val();
     let geboorte_datum = $("#geboorte_datum").val();
-    let gebruiker_id = $("#gebruiker_id").val();
-
-    //de ingevoerde waardes worden in de database gezet
-    FYSCloud.API.queryDatabase(
-        "UPDATE gebruiker SET voornaam = ?, achternaam = ?, geslacht = ?, geboortedatum = ? WHERE gebruiker_id = ?",
-        [voornaam, tussenvoegsel, achternaam, geslacht, geboorte_datum, gebruiker_id]
-    ).done(function(data) {
-        console.log(data[0]);
-    }).fail(function(reason) {
-        console.log(reason);
-    });
-
-}
-function Register() {
-    let voornaam = $("#voornaam").val();
-    let tussenvoegsel = $("#tussenvoegsel").val();
-    let achternaam = $("#achternaam").val();
-    // let salt = salt(32);
-    // achternaam = encrypt(achternaam);
-
-
+    let gebruiker_id = fyscloud.session.get("userId")
 
 
    //de ingevoerde waardes worden in de database gezet
        FYSCloud.API.queryDatabase(
-           "UPDATE gebruiker SET voornaam = ?, tussenvoegsel = ? achternaam = ?, geslacht = ?, geboortedatum = ? WHERE gebruiker_id = ?",
+           "UPDATE gebruiker SET voornaam = ?, tussenvoegsel = ? achternaam = ?, geslacht = ?, geboorte_datum = ?, WHERE gebruiker_id = ?",
            [voornaam, tussenvoegsel, achternaam, geslacht, geboorte_datum, gebruiker_id]
        ).done(function(data) {
            console.log(data);
        }).fail(function(reason) {
            console.log(reason);
        });
-    //de ingevoerde waardes worden in de database gezet
-    FYSCloud.API.queryDatabase(
-        "INSERT INTO gebruiker(voornaam, tussenvoegsel, achternaam, geboortedatum, ) VALUES(?,?,?,?,?,?,?,?,?)",
-        [voornaam, tussenvoegsel, achternaam, email, wachtwoord,  geslacht, geboorte_datum, avatar, bio]
-    ).done(function(data) {
-        console.log(data);
-    }).fail(function(reason) {
-        console.log(reason);
-    });
+
 }
