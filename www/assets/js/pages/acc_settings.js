35e7bd59289e780f26902e67af3a1528640e3f17
ronaraederomasrieza <rona.rieza2393@gmail.com>
2019-12-09 10:35:50 +0100

    includes fixed

--- www/assets/js/pages/acc_settings.js
+++ www/assets/js/pages/acc_settings.js
@@ -1,10 +1,17 @@
 //profiel bewerken
 function editProfile() {
-    //hier komt de variables
+    let voornaam = $("#voornaam").val();
+    let tussenvoegsel = $("#tussenvoegsel").val();
+    let achternaam = $("#achternaam").val();
+    let geslacht = $("#geslacht").val();
+    let geboortedatum = $("#geboortedatum").val();
+
+
+
 
     //de ingevoerde waardes worden in de database gezet
     FYSCloud.API.queryDatabase(
-        "INSERT INTO gebruiker(voornaam, tussenvoegsel, achternaam, geslacht, geboortedatum, profiel_foto, bio) VALUES(?,?,?,?,?,?,?)",
+        "UPDATE gebruiker(voornaam, tussenvoegsel, achternaam, geslacht, geboortedatum) VALUES(?,?,?,?,?)",
         [voornaam, tussenvoegsel, achternaam, geslacht, geboorte_datum, avatar, bio]
     ).done(function (data) {
         console.log(data);
