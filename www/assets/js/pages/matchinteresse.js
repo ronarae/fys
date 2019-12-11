a50d2b4842b225fcfe86044ae6bd2d105ad74e82
basmulder03 <bas.d.mulder@gmail.com>
2019-12-11 11:23:18 +0100

    Compare functie voor arrays

--- www/assets/js/pages/matchinteresse.js
+++ www/assets/js/pages/matchinteresse.js
@@ -1,16 +1,27 @@
 function match() {
-    const interesse1 = $("input[name='blablabla']").val();
-if(interesse1){
+  const interesse1 = $("input[name='blablabla']").val();
+  if (interesse1) {
     console.log("geen waarder");
-}
-else{
+  } else {
     FYSCloud.API.queryDatabase(
-        "SELECT * FROM gebruiker_has_interesses Where interesses_interesses_id = ?",
-        [interesse1]
-    ).done(function (data) {
-        console.log(data);
+      "SELECT * FROM gebruiker_has_interesses Where Gebruiker_gebruiker_id = ?",
+      [FYSCloud.Session.get]
+    ).done(function(data) {
+      console.log(data);
     }).fail(reason => {
-        console.log(reason);
+      console.log(reason);
     });
+  }
 }
+
+function compare(array1, array2) {
+  let equals = 0;
+  for (let i = 0; i < array1.length; i++) {
+    for (let j = 0; j < array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        equals++;
+      }
+    }
+  }
+  return (equals / ((array1.length + array2.length) / 2) * 100);
 }
