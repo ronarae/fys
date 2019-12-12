944cbc8d8459af0abe07acebddb5859ffdce0051
ronaraederomasrieza <rona.rieza2393@gmail.com>
2019-12-12 16:29:57 +0100

    update accountgegevens werkend

--- www/assets/js/pages/updateProfiel.js
+++ www/assets/js/pages/updateProfiel.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {
-    overmijUpdate();
-    profielfotoUpdate();
-});
-
 //update de bio van de gebruiker
 function overmijUpdate () {
     console.log("gewerkt");
@@ -19,14 +14,18 @@ function overmijUpdate () {
     });
 }
 
-function profielfotoUpdate() {
-    console.log("gewerkt");
-    let profiel_foto = $("#profiel_foto").val();
+function accountgegevensUpdate() {
+    console.log("working");
+    let voornaam = $("#voornaam").val();
+    //  let tussenvoegsel = $("#tussenvoegsel").val();
+    let achternaam = $("#achternaam").val();
+    let geslacht = $("#geslacht").val();
+    //let geboorte_datum = $("#geboortedatum").val();
 
     //de ingevoerde waardes worden in de database gezet
     FYSCloud.API.queryDatabase(
-        "UPDATE gebruiker SET bio = ? WHERE gebruiker_id = ?",
-        [profiel_foto, FYSCloud.Session.get('userId')]
+        "UPDATE gebruiker SET voornaam = ?, achternaam = ?, geslacht = ? WHERE gebruiker_id = ?",
+        [voornaam, achternaam, geslacht, FYSCloud.Session.get('userId')]
     ).done(function(data) {
         console.log(data[0]);
     }).fail(function(reason) {
