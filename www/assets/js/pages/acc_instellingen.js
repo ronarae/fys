9578d0e4bc4cacfb6083417ad3f851e031968e8d
Yasmine Amadou <yasmine.amadou@hva.nl>
2019-12-12 16:33:26 +0100

    Delete user werkt, alleen nog de redirect naar de index doen

--- www/assets/js/pages/acc_instellingen.js
+++ www/assets/js/pages/acc_instellingen.js
@@ -1,5 +1,20 @@
 function verwijderAcc() {
-alert("Weet je zeker dat je het account wil verwijderen? Dit kan niet ongedaan worden!");
-window.location="index.html";
-}
+// alert("Weet je zeker dat je het account wil verwijderen? Dit kan niet ongedaan worden!");
 
+    let gebruiker_id = FYSCloud.Session.get("userId");
+
+    console.log("working");
+    //de gebruiker word verwijderd
+    FYSCloud.API.queryDatabase(
+        "DELETE FROM gebruiker WHERE gebruiker_id = ?",
+        [gebruiker_id, FYSCloud.Session.get("userId")]
+    ).done(function(data) {
+        console.log(data[0]);
+    }).fail(function(reason) {
+        console.log(reason);
+    });
+
+    // FYSCloud.Session.remove("userId");
+    // FYSCloud.URL.redirect("index.html");
+
+}
