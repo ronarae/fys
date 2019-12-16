a755fee083f3fb3ab61d226bf55dc09b80d94334
Anies Sayed <aniesjunior@outlook.com>
2019-12-16 13:24:18 +0100

    updateInteresse werkt

--- www/assets/js/pages/updateProfiel.js
+++ www/assets/js/pages/updateProfiel.js
@@ -1,33 +1,37 @@
-//update de bio van de gebruiker
+//update de bio en interesses van de gebruiker
 function overmijUpdate () {
-    console.log("gewerkt");
     let bio = $("#bio").val();
     let interesses = getCheckedCheckboxes($('input.interesses_checkbox:checkbox:checked'));
     //de ingevoerde waardes worden in de database gezet
     FYSCloud.API.queryDatabase(
         "UPDATE gebruiker SET bio = ? WHERE gebruiker_id = ?",
         [bio, FYSCloud.Session.get('userId')]
-    ).done(function(data) {
-        console.log(data[0]);
+    ).done(function() {
+        FYSCloud.API.queryDatabase("DELETE FROM gebruiker_has_interesses WHERE Gebruiker_gebruiker_id = ?", [FYSCloud.Session.get('userId')]).done(function(data){
+        let query = "INSERT INTO gebruiker_has_interesses (Gebruiker_gebruiker_id, Interesses_interesses_id) VALUES ";
+                    for (let i = 0; i < interesses.length; i++) {
+                      if ((i + 1) >= interesses.length) {
+                        query += "('" + FYSCloud.Session.get('userId') + "' , ?)";
+                      } else {
+                        query += "(" + FYSCloud.Session.get('userId') + ", ?), ";
+                      }
+
+                    }
+                    FYSCloud.API.queryDatabase(
+                                            query,
+                                            interesses
+                                        ).done(function(data) {
+                                            console.log(data);
+                                        }).fail(function(reason) {
+                                            console.log(reason);
+                                        });
+        })
+
     }).fail(function(reason) {
         console.log(reason);
     });
-    let query = "DELETE FROM gebruiker_has_interesses WHERE Gebruiker_gebruiker_id = ?;INSERT INTO gebruiker_has_interesses (Gebruiker_gebruiker_id, Interesses_interesses_id) VALUES ";
-    for (let i = 0; i < interesses.length; i++) {
-      if ((i + 1) >= interesses.length) {
-        query += "('" + FYSCloud.Session.get('userId') + "' , ?)";
-      } else {
-        query += "(" + FYSCloud.Session.get('userId') + ", ?), ";
-      }
-    FYSCloud.API.queryDatabase(
-        query,
-        [FYSCloud.Session.get('userId'), ...interesses]
-    ).done(function(data) {
-        console.log(data);
-    }).fail(function(reason) {
-        console.log(reason);
-    });
-    }
+
+
 }
 
 
