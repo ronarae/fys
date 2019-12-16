2fbcef45aee3f559a56ac271914f61a4dc61b52f
Anies Sayed <aniesjunior@outlook.com>
2019-12-16 11:01:18 +0100

    interresse update

--- www/assets/js/pages/updateCheckboxes.js
+++ www/assets/js/pages/updateCheckboxes.js
@@ -2,7 +2,7 @@ function laadAlleVelden(id, tabel) {
   let data_naam;
   let data_id;
   FYSCloud.API.queryDatabase(
-    `SELECT * FROM ${tabel}`
+    `SELECT * FROM interesses ${tabel}`
   ).done(function(data) {
     for (let i = 0; i < data.length; i++) {
       data_id = (tabel == 'interesses') ? data[i].interesses_id : data[i].bestemming_id;
@@ -33,5 +33,47 @@ function getCheckedCheckboxes(selector) {
 
 includeDone(function() {
   laadAlleVelden('#interessesUpdate', 'interesses');
-  laadAlleVelden('#bestemmingen', 'bestemming');
 });
+function laadAlleVelden(id, tabel) {
+  let data_naam;
+  let data_id;
+  FYSCloud.API.queryDatabase(
+    `SELECT * FROM interesses`
+  ).done(function(data) {
+    FYSCloud.API.queryDatabase(
+        `SELECT * FROM gebruiker_has_interesses WHERE Gebruiker_gebruiker_id = ?`,
+        [FYSCloud.Session.get('userId')]
+      ).done(function(more_data) {
+      for (let i = 0; i < data.length; i++) {
+        let equals = false;
+        for (let j = 0; j < more_data.length; j++) {
+          if (more_data[j].Interesses_interesses_id === data[i].interesses_id) {
+            equals = true;
+          }
+        }
+        if (equals) {
+          $(id).append(`
+            <label class="checkbox-inline">
+              <input type="checkbox" class="interesses_checkbox" value="${data[i].interesses_id}" checked />
+              <span class="badge">${data[i].interesse_naam}</span>
+            </label>
+            `);
+        } else {
+          $(id).append(`
+            <label class="checkbox-inline">
+              <input type="checkbox" class="interesses_checkbox" value="${data[i].interesses_id}" />
+              <span class="badge">${data[i].interesse_naam}</span>
+            </label>
+            `);
+        }
+        if ((i % 5) === 0 && i != 0) {
+          $(id).append(`<br>`);
+        }
+      }
+    }).fail(function(reason) {
+    console.log(reason);
+  });
+  }).fail(function(reason) {
+    console.log(reason);
+  });
+}
