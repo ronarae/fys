451bda2dfea2d9930ed3beabd4fdb8963a401401
KinHan <kinhan.wong@hva.nl>
2019-12-27 13:44:10 +0100

    adminpagina klaar

--- www/assets/js/pages/adminloginlogout.js
+++ www/assets/js/pages/adminloginlogout.js
@@ -22,7 +22,10 @@ function loginLogoutadmin() {
       </a>
       <button data-toggle='modal' data-target='#loginModal' class='nav-link text-white btn btn-link'>
       <i class='fas fa-sign-in-alt'></i> Login
-      </button>`
+      </button>
+            <button data-toggle='modal' data-target='#loginModaladmin' class='nav-link text-white btn btn-link'>
+            <i class='fas fa-sign-in-alt'></i> Admin
+            </button>`
         );
     }
 }
