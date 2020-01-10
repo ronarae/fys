33629d997666bca5d6f48b3655befa3bf909a313
Yasmine Amadou <yasmine.amadou@hva.nl>
2020-01-10 14:49:26 +0100

    send email veranderen

--- www/assets/js/pages/SendEmail.js
+++ www/assets/js/pages/SendEmail.js
@@ -1,29 +1,43 @@
 $(document).ready(function() {
- SendEmail();
+ sendEmail();
 });
 
-function SendEmail() {
-
-    let email = $('#email');
+function sendEmail() {
+    //
+    // let email = $('#email');
     let naam = $('#naam');
+    // let interesses = $('#interesses')
+    let email_veld = $('#email-veld');
+    let subjectveld = $('#subjectveld');
 
-    FYSCloud.API.sendEmail({
-        from: {
-            name: "Group 5 Assemble",
-            address: "group@fys.cloud"
-        },
-        to: [
-            {
-                name: naam,
-                address: email
-            }
-        ],
-        subject: "Je bent aangemeld voor Assemble!!!",
-        html: "<h1>Hello Team 5</h1><p>This is an email :)</p>"
-    }).done(function(data) {
+    FYSCloud.API.queryDatabase(
+        "SELECT * FROM gebruiker gebruiker_id = ?", [userId]
+    ).done(function(data) {
         console.log(data);
+        FYSCloud.API.sendEmail({
+            from: {
+                name: "Group 5 Assemble",
+                address: "group@fys.cloud"
+            },
+            to: [
+                {
+                    name: naam,
+                    address: email
+                }
+            ],
+            subject: subjectveld,
+            text: email_veld,
+            html: "<h1>Je bent gematched!!!</h1><p>Je bent gematched met iemand :)</p>"
+        }).done(function(data) {
+            console.log(data);
+        }).fail(function(reason) {
+            console.log(reason);
+        });
+
     }).fail(function(reason) {
         console.log(reason);
+        alert("Er is iets misgegaan")
     });
+
 
 }
